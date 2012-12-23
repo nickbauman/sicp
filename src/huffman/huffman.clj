b1f50a9059b52c058b32f6501d23810a5452854e
Nick Bauman <nick.bauman@cirrusmachina.com>
2012-12-22 21:02:56 -0600

    added doc to each method

--- src/huffman/huffman.clj
+++ src/huffman/huffman.clj
@@ -1,51 +1,63 @@
-(ns huffman)
+(ns huffman
+  (:require [clojure.test :as test]))
 
 (declare symbols symbol-leaf weight)
 
-(defn make-leaf 
+(defn make-leaf
+  "Creates a leaf node from a symbol and its leaf"
 	[symbowl weight]
   (list :leaf symbowl weight))
 
 (defn leaf? 
+  "Returns whether a node is a leaf"
 	[object]
   (= (first object) :leaf))
 
 (defn symbol-leaf 
+  "Returns the symbol of this leaf"
 	[x] 
 	(second x))
 
-(defn weight-leaf 
+(defn weight-leaf
+  "Returns the weight of a given node"
 	[x] 
 	(nth x 2))
 
 (defn left-branch 
+  "Returns the left branch of the current huffman tree"
   [tree] 
 	(first tree))
 
-(defn right-branch 
+(defn right-branch
+  "Returns the right branch of the current huffman tree"
   [tree] 
 	(second tree))
 
-(defn symbols 
+(defn symbols
+  "Returns the current branch's symbol"
 	[tree]
   (if (leaf? tree)
       (list (symbol-leaf tree))
       (nth tree 2)))
 
 (defn weight 
+  "Returns the branch's weight"
 	[tree]
  (println "tree" tree)
  (if (leaf? tree)
    (weight-leaf tree)
    (nth tree 3)))
 
-(defn choose-branch 
+(defn choose-branch
+  "Takes the current branch of a huffman tree and returns the next branch. For decoding."
 	[bit branch]
   (cond (= bit 0) (left-branch branch)
         (= bit 1) (right-branch branch)
         :else (throw (RuntimeException. (str "bad bit -- CHOOSE-BRANCH " bit)))))
 
 (defn encode-symbol
+  "Takes a huffman tree and a token, traverses the tree and returns the branch 
+  directions as a list of traversals to find the token"
   [tree token]
   (let [has-symbol (fn[branch] (some #(= token %) (symbols branch)))] 
     (if (has-symbol tree)
@@ -60,11 +72,16 @@
                    (conj encoded-list 0)))))
       (throw (RuntimeException. (str "huffman tree cannot encode token '" token "'"))))))
 
-(defn encode 
+(defn encode
+  "Returns a seq of 1s and 0s taking a message seq and an adaquate huffman tree
+  for that sequence. Throws an exception if the huffman tree does not contain 
+  a particular element of the message sequence."
   [message tree]
     (mapcat (partial encode-symbol tree) message))
 
 (defn make-code-tree 
+  "Joins two nodes by creating a parent node from a seq of their combined 
+  symbols and the sum of their weights in the tree"
 	[left right]
   (list left
         right
@@ -80,15 +97,6 @@
 
 (defn make-leaf-set 
   [pairs]
-  (if (nil? pairs)
-    ()
-    (let [pair (first pairs)]
-      (adjoin-set (make-leaf (first pair)    ; symbol
-                             (second pair))  ; frequency
-                  (make-leaf-set (rest pairs))))))
-
-(defn make-leaf-set 
-  [pairs]
   (loop [[pair & remaining] pairs
          t ()]
     (if pair
@@ -101,11 +109,15 @@
   ;; TODO implement this (make-code-tree ...
   )
 
-(defn generate-huffman-tree 
+(defn generate-huffman-tree
+  "Takes a set of frequency pairs and generates a huffman tree datastructure from it"
   [pairs]
   (successive-merge (make-leaf-set pairs)))
 
 (defn decode
+  "Decode numeric bits represented as either a 1 (right branch) or a 0 (left 
+  branch) traversing to a left or a right node util a terminal node is 
+  reached, which is the character to convert to."
   [bits tree]
   (loop [my-bits bits
          current-branch tree
@@ -117,6 +129,13 @@
           (recur (rest my-bits) tree (conj decoded-seq (symbol-leaf next-branch)))
           (recur (rest my-bits) next-branch decoded-seq))))))
 
+
+;;;;;;
+;; End of Huffman implementation. The rest of this is code to exercise it.
+;;;;;;
+
+;; Some frobbing around with huffman
+
 (def sample-tree
   (make-code-tree (make-leaf \A 4)
                   (make-code-tree
@@ -126,9 +145,30 @@
 
 (def sample-message (list 0 1 1 0 0 1 0 1 0 1 1 1 0))
 
-(time (with-open [rdr (java.io.BufferedReader. 
-                 (java.io.FileReader. "/Users/nickbauman/no_sql_non-starter.txt"))]
-  (let [seq (line-seq rdr)
-        str-seq (apply str seq)]
-    (frequencies str-seq))))
+(def sample-decoded-message [\A \D \A \B \B \C \A])
+
+(test/deftest test-simple-decode
+              (test/is (= sample-decoded-message (decode sample-message sample-tree))))
+
+(test/deftest test-simple-encode
+              (test/is (= sample-message (encode sample-decoded-message sample-tree))))
+
+;; A mechanism to generate random strings for testing huffman trees
+
+;; A sample set of characters with dupes to influence commonly used characters in English
+(def keyboard "`1234567890-=qweeeertttyuuuiiiioooopp[]\\aaassssssddffghhjjkl;'zxccvbbnnmm,./        ")
+
+(defn sel-rand-char
+  "Select a random character from string"
+  [string]
+  (nth string (int (rand (count keyboard)))))
+
+(defn gen-rand-str
+  "Create a random string of a certain size that relies on 'keyboard' value for a sample gamut"
+  [size]
+  (apply str (map (fn[x] (sel-rand-char keyboard)) (range size))))
+
+(def sample-string (gen-rand-str 6000))
+
+(def sample-frequencies (sort (comparator (fn[[a x] [b y]] (> x y))) (frequencies sample-string)))
 
