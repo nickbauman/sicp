341e192c8b78c6473a861ecf936cd8959b4322d1
Nick Bauman <nick.bauman@cirrusmachina.com>
2013-01-30 20:49:52 -0600

    added exercises through 1.1.3

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject scip "0.1.0-SNAPSHOT"
+  :description "Structure and Interpretation of Computer Programs, Exercises in Clojure"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/math.numeric-tower "0.0.2"]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]]
+  :jvm-opts ["-server"])
--- src/section1_1_6/conditional_expressions_and_predicates.clj
+++ src/section1_1_6/conditional_expressions_and_predicates.clj
@@ -1,20 +1,22 @@
-(ns section1-1-6.conditional-expressions-and-predicates)
+(ns section1-1-6.conditional-expressions-and-predicates
+  (:require [clojure.math.numeric-tower :as math]))
 
 (println "Exercise 1.1.1")
+(println "Below is a sequence of expressions. What is the result printed by the interpreter in response to each expression?\nAssume that the sequence is to be evaluated in the order in which it is presented.")
 
 (def exercise111-col
-  ["10\n"
-   "(+ 5 3 4)\n"
-   "(- 9 1)\n"
-   "(/ 6 2)\n"
-   "(+ (* 2 4) (- 4 6))\n"
-   "(def a 3)\n"
-   "(def b (+ a 1))\n"
-   "(+ a b (* a b))\n"
-   "(= a b)\n"
-   "(if (and (> b a) (< b (* a b))) b a)\n"
-   "(cond (= a 4) 6 (= b 4) (+ 6 7 a) :else 25)\n"
-   "(+ 2 (if (> b a) b a))\n"
+  ["10"
+   "(+ 5 3 4)"
+   "(- 9 1)"
+   "(/ 6 2)"
+   "(+ (* 2 4) (- 4 6))"
+   "(def a 3)"
+   "(def b (+ a 1))"
+   "(+ a b (* a b))"
+   "(= a b)"
+   "(if (and (> b a) (< b (* a b))) b a)"
+   "(cond (= a 4) 6 (= b 4) (+ 6 7 a) :else 25)"
+   "(+ 2 (if (> b a) b a))"
    "(* (cond (> a b) a (< a b) b :else -1) (+ a 1))"])
 
 (doall 
@@ -25,5 +27,16 @@
        exercise111-col))
 
 (println "Exercise 1.1.2")
+(println "Translate the following expression [see text] into prefix form")
 
-(double (/ (+ 5 1 (- 2 3 (+ 6 (/ 1 5)))) (* 3 (- 6 2) (- 2 7)))) 
+(println (double (/ (+ 5 1 (- 2 3 (+ 6 (/ 1 5)))) (* 3 (- 6 2) (- 2 7)))))
+
+(println "Exercise 1.1.3")
+(println "Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers")
+
+(defn procedure
+  [x y z]
+  (let [[_ a b] (sort [x y z])]
+    (+ (* a a) (* b b))))
+
+(println (procedure 2 5 3))
